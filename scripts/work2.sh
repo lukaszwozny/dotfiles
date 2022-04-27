67d362443d9735e877355820c167255b81e6e3b0
Łukasz Woźny <kontakt@lukaszwozny.com>
2022-04-27 21:02:30 +0200

    Added enable_all script

---
+++ scripts/work2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wmctrl -s 1
+code ~/Projects/composed/ldek/leknanuxt &
+# code ~/Documents/flutter/LekNaSmartfony &
+sleep 1
+wmctrl -s 2
+code ~/Documents/python/LekNaServer &
+sleep 1
+gnome-terminal -- /bin/sh -c './development/redis/redis-server; exec bash' &
+sleep 1
+wmctrl -s 3
+code ~/Documents/python/potrace_icon &
+sleep 1
+wmctrl -s 4
+code ~/Projects/Python/xlsx2lang
+sleep 1
+wmctrl -s 1
